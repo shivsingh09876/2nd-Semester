2ae9c6d86d4bd222156aeab5cf1f49dc6c743684
shivsingh09876 <146877195+shivsingh09876@users.noreply.github.com>
2024-03-03 22:30:38 +0530

    Add files via upload

---
+++ program-2.c
@@ -0,0 +1,13 @@
+// write a program to convert upper case letter to lower case letter
+// by using bitwise operator
+
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter A Alphabet :");
+    scanf("%c", &ch);
+    ch = ch | 32;
+    printf("%c", ch);
+    return 0;
+}
